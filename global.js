6ab3b60bdd078b1a453e3bacc735431c7d91c9db
Igor <igor.semenyuk.canada@gmail.com>
2023-11-27 01:05:47 -0500

    lesson 3, working with object proces.argv - it lets us build command line interface, look at nodejs.org/api/globals.html

--- global.js
+++ global.js
@@ -6,6 +6,17 @@ console.log(__dirname);
 console.log(__filename);
 console.log(`The file name is ${path.basename(__filename)}`);
 
-for (let key in global) {
-    console.log(key);
+
+/**  runing mode with arguments  
+ * node global.js --user Eve --greeting "Hello"   
+*/
+
+function grab(flag) {
+    let indexAfterFlag = process.argv.indexOf(flag) + 1;
+    return process.argv[indexAfterFlag];
 }
+
+let greeting = grab("--greeting");
+let user = grab("--user");
+
+console.log(greeting, user);
