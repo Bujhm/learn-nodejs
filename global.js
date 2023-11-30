5f82915485aff5121cc585c672d7a97a677d21ea
Igor <igor.semenyuk.canada@gmail.com>
2023-11-30 16:28:34 -0500

    lesson 6 and 7 using setTimeout, setInvervl and clearInterval

--- global.js
+++ global.js
@@ -1,57 +1,18 @@
-const path = require('path');
-
-let hello = "Hello World from Node.js"
-let justnode = hello.slice(17);
-console.log(__dirname);
-console.log(__filename);
-console.log(`The file name is ${path.basename(__filename)}`);
-
-
-/**  runing mode with arguments  
- * node global.js --user Eve --greeting "Hello"   
-*/
-
-function grab(flag) {
-    let indexAfterFlag = process.argv.indexOf(flag) + 1;
-    return process.argv[indexAfterFlag];
+// to run this script use: node global
+const waitTime = 3000;
+console.log(`setting a ${waitTime / 1000} second delay`);
+const timerFinished = () => {
+    clearInterval(interval);
+    console.log("done");
 }
 
-let greeting = grab("--greeting");
-let user = grab("--user");
+setTimeout(timerFinished, waitTime);
 
-console.log(greeting, user);
+const waitInterwal = 500;
+let currentTime = 0;
+const incTime = () => {
+    currentTime += waitInterwal;
+    console.log(`waiting ${currentTime / 1000} seconds`);
+};
 
-// using process.stdout , to run this script use  node global
-process.stdout.write("Hello World \n \n");
-
-const questions = [
-    "What is your name?",
-    "Ehat is your favorite color?",
-    "What is your favorite food?",
-    "What is your favorite movie?"
-];
-
-const answers = [];
-
-function ask(i = 0) {
-    process.stdout.write(`\n\n\n ${questions[i]}`);
-    process.stdout.write(` > `);
-}
-
-ask();
-
-// here we are using process.stdin to get the data from the user using event "on"
-process.stdin.on("data", (data) => {
-    answers.push(data.toString().trim() + "\n");
-    if (answers.length < questions.length) {
-        ask(answers.length);
-    } else {
-        process.exit();
-    }
-})
-
-process.on("exit", () => {
-    process.stdout.write("\n\n\n");
-    process.stdout.write("Thank you for your answers\n");
-    process.stdout.write(answers.join(""));
-})
+const interval = setInterval(incTime, waitInterwal);
