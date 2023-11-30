f110f81503dd93f7f1d88bf4c7dfc35e5872fcd7
Igor <igor.semenyuk.canada@gmail.com>
2023-11-30 00:47:45 -0500

    lesson 4 - start building small questionary, part1

--- global.js
+++ global.js
@@ -20,3 +20,27 @@ let greeting = grab("--greeting");
 let user = grab("--user");
 
 console.log(greeting, user);
+
+// using process.stdout , to run this script use  node global
+process.stdout.write("Hello World \n \n");
+
+const questions = [
+    "What is your name?",
+    "Ehat is your favorite color?",
+    "What is your favorite food?",
+    "What is your favorite movie?"
+];
+
+const answers = [];
+
+function ask(i) {
+    process.stdout.write(`\n\n\n ${questions[i]}`);
+    process.stdout.write(` > `);
+}
+
+// here we are using process.stdin to get the data from the user using event "on"
+process.stdin.on("data", (data) => {
+    process.stdout.write(data.toString().trim() + "\n");
+})
+
+ask(answers.length);
