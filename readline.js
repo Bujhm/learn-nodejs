0bea7bef5f246cb525a0a1f1c5aeff082a6a4efc
Igor <igor.semenyuk.canada@gmail.com>
2023-12-02 16:38:28 -0500

    lesson 3.6 - creating a mousle, refactoring readline.js former ask.js

--- readline.js
+++ readline.js
@@ -1,9 +1,4 @@
-const readline = require("readline");
-const { resourceLimits } = require("worker_threads");
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+const collectAnswers = require("./lib/collectAnswers");
 
 const questions = [
     "What is your name?",
@@ -12,32 +7,8 @@ const questions = [
     "What is your favorite movie?"
 ];
 
-/*
- * This function will collect answers to the questions
- */
-function collectAnswers(questions, done) {
-    const answers = [];
-
-    /*
-    * This function will be called when a question is answered it uses the _recursion_ to ask the next question
-    */
-    const questionAnswered = (answer) => {
-        answers.push(answer.trim());
-        if (answers.length < questions.length) {
-            rl.question(
-                questions[answers.length],
-                questionAnswered
-            );
-        } else {
-            return done(answers);
-        }
-    }
-    rl.question(questions[0], questionAnswered);
-}
-
 collectAnswers(questions, (answers) => {
     console.log("Thank you for your answers");
     console.log(answers);
     process.exit();
 });
-
