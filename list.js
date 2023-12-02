1aa291eed15502fff8abda9a08d841276a39e5d4
Igor <igor.semenyuk.canada@gmail.com>
2023-12-02 18:25:47 -0500

    lesson 4.1, listing directory files part 2 , non  blocking

--- list.js
+++ list.js
@@ -1,5 +1,12 @@
 const fs = require("fs");
 
-let files = fs.readdirSync("./"); //it blocks execution until it gets the result
+// this is non blocking variant of fs.readdirSync
+fs.readdir("./", function (err, files) {
+    if (err) {
+        throw err;
+    }
 
-console.log(files);
+    console.log(files);
+});
+
+console.log("reading files ...");
