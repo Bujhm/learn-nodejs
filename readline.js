c3837fb04a83b4bc5bff01e930e020ac3347eec9
Igor <igor.semenyuk.canada@gmail.com>
2023-12-02 16:55:24 -0500

    lesson 3.7 - consuming a module with EventEmitter (refactoring)

--- readline.js
+++ readline.js
@@ -2,13 +2,15 @@ const collectAnswers = require("./lib/collectAnswers");
 
 const questions = [
     "What is your name?",
-    "Ehat is your favorite color?",
+    "What is your favorite color?",
     "What is your favorite food?",
     "What is your favorite movie?"
 ];
 
-collectAnswers(questions, (answers) => {
+const answerEvents = collectAnswers(questions, (answers) => {
     console.log("Thank you for your answers");
     console.log(answers);
     process.exit();
 });
+
+answerEvents.on("answer", (answer) => console.log(`you answered: ${answer}`));
